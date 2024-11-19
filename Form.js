bd1db42d4595cbf69daf8eaeb23184f98e960b50
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-19 11:39:28 +0100

    first commit

---
+++ Form.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Form() {
+    const [firstName, setFirstName] = React.useState("")
+    
+    alert(firstName)
+    
+    function handleChange(event) {                /**
+         * Challenge: update the firstName state on every keystroke
+         */       
+        
+        setFirstName(event.target.value);
+        
+
+    }
+    
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}
+            />
+        </form>
+    )
+}
